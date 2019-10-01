73522b4947e94356159e9d75f7160bed619b412c
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2019-10-01 17:41:41 +0100

    Add definition for instances

---
+++ instances.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "amzn_linux_2" {
+    most_recent = true
+
+    filter {
+        name = "Name"
+        values = "amzn2-ami-hvm-2.0.*ebs" # Amazon Linux 2, EBS Storage AMI
+    }
+}
+
+resource "aws_instance" "instance_1" {
+    ami = "${data.aws_ami.amzn_linux_2.id}"
+    subnet_id = "${aws_subnet.subnet_1.id}"
+    vpc_security_group_ids = "${aws_security_group.sg-1.id}"
+    key_name = "deniz-project-b-key" # TODO: Consider moving this out to a variable?
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "deniz-project-b-ec2-1"
+    }
+}
+
