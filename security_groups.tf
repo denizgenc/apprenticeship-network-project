e4e9781015ec28d15d506d895e1bdd7115c14b07
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2019-10-01 16:53:58 +0100

    Add definition for Security Groups
    Not completely sure whether the cidr_blocks will work, for two reasons:
        - May be that Amazon won't automatically expand this into a list of
        ingress rules
        - May also be that Terraform itself doesn't like lists being spread
        over multiple lines
    
    Also I took code from the docs to replicate Amazon's automatic creation
    of an egress rule

---
+++ security_groups.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "sg-1" {
+    name = "deniz-project-b-sg-1"
+    vpc_id = "${aws_vpc.vpc_1.id}"
+
+    ingress {
+        description = "SSH ingress from GDS IPs"
+        from_port = "22"
+        to_port = "22"
+        cidr_blocks = ["213.86.153.212/32", "213.86.153.213/32", "213.86.153.214/32",
+                       "213.86.153.235/32", "213.86.153.236/32", "213.86.153.237/32",
+                       "85.133.67.244/32"]
+    }
+    
+    # AWS default egress rule, replicated here (see the bottom of:
+    # https://www.terraform.io/docs/providers/aws/r/security_group.html#argument-reference )
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
