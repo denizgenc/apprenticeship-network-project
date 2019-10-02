b518374239acbf6052c2c568245cac04b8774d2e
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2019-10-02 12:01:16 +0100

    Add definition for Elastic IPs
    Make sure to:
    - Make the EIP depend on the IGW to make sure the IGW gets created
      first, since it needs the IGW to be present before the EIP can be
      associated with an instance
    - associate an Elastic IP as part of its creation - Amazon charges for
      unassociated EIPs, so don't let them lie around.

---
+++ eips.tf
@@ -0,0 +1,8 @@
+resource "aws_eip" "eip_1" {
+    depends_on = ["aws_internet_gateway.igw_1"]
+    vpc = true
+    instance = "${aws_instance.instance_1.id}"
+    tags = {
+        Name = "${var.name_prefix}eip-1"
+    }
+}
