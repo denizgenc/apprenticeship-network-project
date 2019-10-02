18ce3ee897622e7bce3c5c74db017bde57182d57
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2019-10-02 15:25:02 +0100

    Add HTTP/S ingress rules to security group

--- security_groups.tf
+++ security_groups.tf
@@ -11,7 +11,27 @@ resource "aws_security_group" "sg_1" {
                        "213.86.153.235/32", "213.86.153.236/32", "213.86.153.237/32",
                        "85.133.67.244/32"]
     }
+
+    ingress {
+        description = "HTTP ingress - only from GDS for now"
+        protocol = "tcp"
+        from_port = "80"
+        to_port = "80"
+        cidr_blocks = ["213.86.153.212/32", "213.86.153.213/32", "213.86.153.214/32",
+                       "213.86.153.235/32", "213.86.153.236/32", "213.86.153.237/32",
+                       "85.133.67.244/32"]
+    }
     
+    ingress {
+        description = "HTTPS ingress - only from GDS for now"
+        protocol = "tcp"
+        from_port = "443"
+        to_port = "443"
+        cidr_blocks = ["213.86.153.212/32", "213.86.153.213/32", "213.86.153.214/32",
+                       "213.86.153.235/32", "213.86.153.236/32", "213.86.153.237/32",
+                       "85.133.67.244/32"]
+    }
+
     # AWS default egress rule, replicated here (see the bottom of:
     # https://www.terraform.io/docs/providers/aws/r/security_group.html#argument-reference )
     egress {
