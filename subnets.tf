c13ac44a11a42c248e0cdc47b470fadd47ab58fd
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2020-02-14 18:16:20 +0000

    Modify subnets to support DB Subnet Groups
    Also:
    - Separate out subnet for web server
    - Increase the amount of IPs available for "subnet 1" from an /18 block
      to a /17 block

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,29 @@
-resource "aws_subnet" "subnet_1" {
+resource "aws_subnet" "subnet_web" {
     vpc_id = "${aws_vpc.vpc_1.id}"
-    cidr_block = "192.168.128.0/18"
+    cidr_block = "192.168.0.0/17"
     availability_zone = "${var.aws_region}a" # e.g. us-east-1a
 
     tags = {
-        Name = "${var.name_prefix}-subnet-1-1" # VPC 1, subnet 1
+        Name = "${var.name_prefix}-subnet-1-1" 
     }
 }
 
-resource "aws_subnet" "subnet_2" {
+resource "aws_subnet" "subnet_db_1" {
     vpc_id = "${aws_vpc.vpc_1.id}"
-    cidr_block = "192.168.192.0/18"
-    availability_zone = "${var.aws_region}b" # e.g. us-east-1b
+    cidr_block = "192.168.128.0/18"
+    availability_zone = "${var.aws_region}a"
 
     tags = {
-        Name = "${var.name_prefix}-subnet-1-2" # VPC 1, subnet 1
+        Name = "${var.name_prefix}-subnet-db-1"
+    }
+}
+
+resource "aws_subnet" "subnet_db_2" {
+    vpc_id = "${aws_vpc.vpc_1.id}"
+    cidr_block = "192.168.192.0/18"
+    availability_zone = "${var.aws_region}b"
+
+    tags = {
+        Name = "${var.name_prefix}-subnet-db-2"
     }
 }
