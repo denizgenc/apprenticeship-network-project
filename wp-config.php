67f157a2f91526d79823ff1fd214030805a97ad7
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2020-03-06 19:12:48 +0000

    Add configuration files for wordpress

---
+++ wp-config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('DB_USER', $_SERVER['RDS_DB_USER']);
+define('DB_PASSWORD', $_SERVER['RDS_DB_PASSWORD']);
+define('DB_HOST', $_SERVER['MYSQL_HOST']);
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
